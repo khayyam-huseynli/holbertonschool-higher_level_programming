2e603777375fc5d3278f36cebce0bbadf274bdc1
root <khayyam.huseynli@gmail.com>
2024-05-17 06:53:25 +0000

    used __import__

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-import add_0
-
 a = 1
 b = 2
 
 if __name__ == "__main__":
-    result = add_0.add(a, b)
+    result = __import__('add_0').add(a, b)
     print("{} + {} = {}".format(a, b, result))
