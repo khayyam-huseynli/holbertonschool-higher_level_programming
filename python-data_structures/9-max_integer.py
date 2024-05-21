0776a9a8d50b78c9e899a9f590e00735ff5eaa30
root <khayyam.huseynli@gmail.com>
2024-05-21 06:36:07 +0000

    first try

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = 0
+    for i in my_list:
+        if i > max:
+            max = my_list[i]
+    return max
