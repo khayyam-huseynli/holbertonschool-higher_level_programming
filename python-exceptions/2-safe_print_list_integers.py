4e4757ba304869ff4592a05360cc02eff691adc2
root <khayyam.huseynli@gmail.com>
2024-05-22 12:24:13 +0000

    removed try/except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return count
+    count = 0
+    for i in range(x):
+        if isinstance(my_list[i], int):
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+    print()
+    return count
