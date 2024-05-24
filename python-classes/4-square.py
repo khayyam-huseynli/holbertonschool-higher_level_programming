9f9c3f0b2f51521f57caae2782ef5ab46d436ecf
root <khayyam.huseynli@gmail.com>
2024-05-24 12:44:28 +0000

    bug fixed

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
 
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif vale < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
