43649f2c24b5179fad35b1209eaf07ba371b5117
khayyam-huseynli <khayyam.huseynli@gmail.com>
2024-06-02 00:02:40 +0400

    updated

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,15 +9,14 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     """
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
-
     if not isinstance(m_a, list):
         raise TypeError("Scalar operands are not allowed, use '*' instead")
     if not isinstance(m_b, list):
         raise TypeError("Scalar operands are not allowed, use '*' instead")
+
+    # Convert lists to NumPy arrays
+    m_a_num = np.array(m_a)
+    m_b_num = np.array(m_b)
 
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
@@ -36,7 +35,12 @@ def lazy_matrix_mul(m_a, m_b):
     if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("setting an array element with a sequence.")
 
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("shapes (2,3) and (2,2) not aligned: 3 (dim 1) != 2 (dim 0)")
+    m_a_num_0, m_a_num_1 = m_a_num.shape()
+    m_b_num_0, m_b_num_1 = m_b_num.shape()
+
+    if m_a_num_1 != m_b_num_0:
+        raise ValueError("shapes {} and {} not aligned: {} (dim 1) != {} \
+                         (dim 0)".format(m_a_num.shape(), m_b_num.shape(),
+                                         m_a_num_1, m_b_num_0))
 
     return (np.matmul(m_a, m_b))
