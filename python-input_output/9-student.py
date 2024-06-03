4e8d8c0f0d6cf70c48d0f6d3ca6174bb7026f640
root <khayyam.huseynli@gmail.com>
2024-06-03 11:01:07 +0000

    bug fixed

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -21,11 +21,11 @@ class Student:
             Function that returns the JSON representation
             of an instance of a Class
         """
-        if not hasattr(obj, '__dict__'):
+        if not hasattr(self, '__dict__'):
             return None  # Not an instance of a class with attributes
 
         json_dict = {}
-        for attr_name, attr_value in obj.__dict__.items():
+        for attr_name, attr_value in self.__dict__.items():
             if isinstance(attr_value, (list, dict, str, int, bool)):
                 json_dict[attr_name] = attr_value
 
