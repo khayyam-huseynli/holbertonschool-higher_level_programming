a8bcfd48c5293e33a24c6c6f835ae9c287f49f1c
root <khayyam.huseynli@gmail.com>
2024-06-03 10:38:50 +0000

    used iteration,obj.__dict__.items()

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -11,6 +11,12 @@ def class_to_json(obj):
         Function that returns the JSON representation
         of an instance of a Class
     """
+    if not hasattr(obj, '__dict__'):
+        return None  # Not an instance of a class with attributes
 
-    import json
-    return json.dumps(obj.to_json())
+    json_dict = {}
+    for attr_name, attr_value in obj.__dict__.items():
+        if isinstance(attr_value, (list, dict, str, int, bool)):
+            json_dict[attr_name] = attr_value
+
+    return json_dict
