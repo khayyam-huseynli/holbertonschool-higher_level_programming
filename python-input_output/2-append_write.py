a655a31c2979999bdeeb23d80f83c50104be0153
root <khayyam.huseynli@gmail.com>
2024-06-03 06:06:24 +0000

    bug fixed

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,7 +5,7 @@
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Function that appends a string at the end of a text file """
 
     with open(filename, 'a', encoding="utf-8") as f:
