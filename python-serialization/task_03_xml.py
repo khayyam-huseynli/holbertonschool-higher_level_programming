f170307ef2ce8665b76ab44488f649ab890af7a5
root <khayyam.huseynli@gmail.com>
2024-06-04 10:47:52 +0000

    dict-xml

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    serialization and deserialization using XML as an
+    alternative format to JSON
+"""
+
+
+def serialize_to_xml(dictionary, filename):
+    import xml.etree.ElementTree as ET
+
+    root = ET.Element('data')
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    import xml.etree.ElementTree as ET
+
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    data = {}
+    for child in root:
+        data[child.tag] = convert_value(child.text)
+    return data
+
+
+def convert_value(value):
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value
